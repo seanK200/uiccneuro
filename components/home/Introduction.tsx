4a68644b6bcd87a282e9fec1b42cebf3e3c41ef4
Youngwoo Kim <yw.sean.kim@gmail.com>
2023-08-31 13:54:08 +0900

    refactor/separate home-intro

---
+++ components/home/Introduction.tsx
@@ -0,0 +1,29 @@
+export default function Introduction() {
+  return (
+    <>
+      <p className="leading-relaxed mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada
+        lectus justo, sed aliquam erat placerat eu. Morbi id lectus ut lectus
+        faucibus consequat. Ut malesuada laoreet sodales. Nulla quis ante enim.
+        In sed vestibulum mi, quis tincidunt ex. Fusce interdum at tellus id
+        lacinia. Maecenas id felis sit amet nunc gravida convallis tincidunt non
+        ante. Curabitur elementum, purus nec ultrices feugiat, nunc sapien
+        tristique sem, ac accumsan mauris nisl quis lacus. Vivamus accumsan
+        nulla ut condimentum convallis.
+      </p>
+      <p className="leading-relaxed mb-4">
+        Integer efficitur, ante quis sodales dignissim, tortor mi lacinia
+        mauris, at semper quam arcu non quam. Nam egestas molestie faucibus.
+        Phasellus id odio congue, accumsan enim quis, aliquet turpis.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Maecenas in quam enim. Aenean at pharetra
+        velit. Vivamus a felis ante. Phasellus sed pharetra odio. Integer
+        condimentum, leo a ultricies hendrerit, est mi tempus neque, eget
+        aliquam nunc risus non velit. In euismod dui nunc, venenatis tempus nisl
+        viverra at. Praesent sapien ex, tincidunt semper iaculis et, accumsan
+        vel ante. Proin rhoncus dolor consectetur leo dictum congue. Cras
+        accumsan at risus quis maximus. Ut efficitur ligula in tempor tempus.
+      </p>
+    </>
+  );
+}
